9233dc76714079ac31157aaa53430f7681a02c81
ivo <ivoheck99@gmail.com>
2025-05-24 15:37:35 +0200

    feat: update terminal output layout

--- lib/src/widgets/downloader/download_terminal_output.dart
+++ lib/src/widgets/downloader/download_terminal_output.dart
@@ -15,7 +15,6 @@ class _DownloadTerminalOutputState extends State<DownloadTerminalOutput> {
   final ScrollController _scrollController = ScrollController();
   bool _shouldAutoScroll = true;
   bool isVisible = false;
-  bool _showCopyButton = false;
 
   @override
   void didUpdateWidget(covariant DownloadTerminalOutput oldWidget) {
@@ -56,48 +55,81 @@ class _DownloadTerminalOutputState extends State<DownloadTerminalOutput> {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextButton(
-            onPressed: () {
-              setState(() {
-                isVisible = !isVisible;
-
-                if (isVisible) {
-                  Future.delayed(const Duration(milliseconds: 50), () {
-                    if (mounted && isVisible) {
-                      setState(() => _showCopyButton = true);
-                    }
-                  });
-                } else {
-                  _showCopyButton = false;
-                }
-              });
-            },
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 8.0),
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(12.0),
+                topRight: Radius.circular(12.0),
+              ),
+              color: isVisible ? Colors.grey.shade700 : Colors.transparent,
             ),
-            child: isVisible
-                ? Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(context.t('Close details')),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(Icons.keyboard_arrow_up),
-                      ),
-                    ],
-                  )
-                : Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(Icons.info_outline),
-                      ),
-                      Text(context.t('Show details')),
-                    ],
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isVisible = !isVisible;
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 8),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      overlayColor: Colors.transparent,
+                    ),
+                    child: isVisible
+                        ? Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(context.t('Close details')),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.keyboard_arrow_up,
+                                ),
+                              ),
+                            ],
+                          )
+                        : Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(Icons.info_outline),
+                              ),
+                              Text(context.t('Show details')),
+                            ],
+                          ),
                   ),
+                ),
+                if (isVisible)
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: IconButton(
+                        onPressed: () {
+                          Clipboard.setData(
+                            ClipboardData(
+                              text: widget.outputLines.join('\n'),
+                            ),
+                          );
+                        },
+                        iconSize: 17.0,
+                        constraints: const BoxConstraints(),
+                        icon: Icon(
+                          Icons.copy,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
         Padding(
@@ -108,8 +140,11 @@ class _DownloadTerminalOutputState extends State<DownloadTerminalOutput> {
                 duration: const Duration(milliseconds: 150),
                 height: isVisible ? 200 : 0,
                 width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(12.0),
+                    bottomRight: Radius.circular(12.0),
+                  ),
                   color: Colors.black,
                 ),
                 child: SingleChildScrollView(
@@ -122,40 +157,6 @@ class _DownloadTerminalOutputState extends State<DownloadTerminalOutput> {
                     ),
                   ),
                 ),
-              ),
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 100),
-                switchInCurve: Curves.easeIn,
-                switchOutCurve: Curves.easeOut,
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return FadeTransition(opacity: animation, child: child);
-                },
-                child: _showCopyButton
-                    ? Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: Alignment.topRight,
-                          child: IconButton(
-                            onPressed: () {
-                              Clipboard.setData(
-                                ClipboardData(
-                                  text: widget.outputLines.join('\n'),
-                                ),
-                              );
-                            },
-                            style: IconButton.styleFrom(
-                              backgroundColor: Colors.grey.shade600,
-                            ),
-                            iconSize: 16.0,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(
-                              Icons.copy,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      )
-                    : const SizedBox.shrink(),
               ),
             ],
           ),
